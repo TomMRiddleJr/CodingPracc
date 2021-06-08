856157f38237ec4b3b8e9a9d00a216e11e569c63
TomMRiddleJr <83856373+TomMRiddleJr@users.noreply.github.com>
2021-06-08 14:10:02 +0530

    Minor Change (#2)

--- InterviewBit/src/imports/TakeSingle.java
+++ InterviewBit/src/imports/TakeSingle.java
@@ -4,24 +4,20 @@ import java.util.Scanner;
 
 public class TakeSingle {
 	
+	private static Scanner sc = new Scanner(System.in);
+	
 	public static int oneInt() {
-		Scanner sc = new Scanner(System.in);
-		
 		int x = sc.nextInt();
-		
-		sc.close();
-		
 		return x;
 	}
 	
 	public static String oneString() {
-		Scanner sc = new Scanner(System.in);
-		
 		String x = sc.nextLine();
-		
-		sc.close();
-		
 		return x;
+	}
+	
+	{
+		sc.close();
 	}
 
 }
