d368f9912456cc1afcc6adebcaf6040666f483dc
Tom Riddle Jr <jr.tomriddle@gmail.com>
2021-07-27 20:39:45 +0530

    First: interviewbit.com/problems/nearest-smaller-element/

---
+++ InterviewBit/src/stacksAndQueues/NearestSmallerElement.java
@@ -0,0 +1,49 @@
+package stacksAndQueues;
+
+import java.util.*;
+import imports.*;
+
+public class NearestSmallerElement {
+
+	public static ArrayList<Integer> prevSmaller(ArrayList<Integer> A) {
+		ArrayList<Integer> ans = new ArrayList<>();
+		Stack<Integer> s = new Stack<>();
+
+		/*
+		 * Shorter way of writing the same loop as below
+		 */
+		for (int i : A) {
+
+			while (!s.isEmpty() && i <= s.peek())
+				s.pop();
+
+			if (s.isEmpty())
+				ans.add(-1);
+			else
+				ans.add(s.peek());
+
+			s.add(i);
+		}
+
+		/*
+		 * 
+		 * My way of writing loop - has a few redundancies. Same concept as above. for
+		 * (int a : A) { if (s.empty()) ans.add(-1); else { if (a > s.peek())
+		 * ans.add(s.peek()); else { while (!s.empty() && s.peek() >= a) s.pop();
+		 * 
+		 * if (s.empty()) ans.add(-1); else ans.add(s.peek()); } }
+		 * 
+		 * s.add(a); }
+		 */
+
+		return ans;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(prevSmaller((ArrayList<Integer>) TakeMultiple.Int.intList(" ")));
+
+	}
+
+}
