6edce733e547c981634c7f1480e47ccfaaa17225
Tom Riddle Jr <jr.tomriddle@gmail.com>
2021-08-29 17:21:06 +0530

    5: interviewbit.com/problems/longest-palindromic-subsequence/

---
+++ InterviewBit/src/dp/LPS.java
@@ -0,0 +1,30 @@
+package dp;
+
+import imports.*;
+
+public class LPS {
+
+	public static int solve(String A) {
+		String B = new StringBuilder(A).reverse().toString();
+		int[][] t = new int[A.length() + 1][B.length() + 1];
+
+		for (int i = 0; i < A.length() + 1; i++)
+			for (int j = 0; j < B.length() + 1; j++)
+				if (i == 0 || j == 0)
+					t[i][j] = 0;
+				else if (A.charAt(i - 1) == B.charAt(j - 1))
+					t[i][j] = 1 + t[i - 1][j - 1];
+				else
+					t[i][j] = Math.max(t[i][j - 1], t[i - 1][j]);
+
+		return t[A.length()][B.length()];
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(solve(TakeSingle.oneString()));
+
+	}
+
+}
